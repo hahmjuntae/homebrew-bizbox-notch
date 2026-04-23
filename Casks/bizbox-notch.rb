8741fb5265e73453ef1251e49420320e100fb95b
hahmjuntae <hahmjuntae@gmail.com>
2026-04-23 10:13:42 +0900

    Update Bizbox Notch to 0.2.27

--- Casks/bizbox-notch.rb
+++ Casks/bizbox-notch.rb
@@ -1,6 +1,6 @@
 cask "bizbox-notch" do
-  version "0.2.26"
-  sha256 "b9ca08985421fa0815f2c09163aefdadd831693c3a547eac2c99e265709a1e9e"
+  version "0.2.27"
+  sha256 "3a66b72adb3406e64fa46cf942664f1e7830371a6782f2e72adffd5790f3b58d"
 
   url "https://github.com/hahmjuntae/bizbox-notch/releases/download/v#{version}/Bizbox-Notch-#{version}.dmg"
   name "Bizbox Notch"
