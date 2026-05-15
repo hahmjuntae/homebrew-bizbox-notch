e05637843bcb9bb9de7b3eceed2aa84586eaf02a
hahmjuntae <hahmjuntae@gmail.com>
2026-05-15 15:07:46 +0900

    chore: Bizbox Notch 0.2.33 배포
    - `bizbox-notch` cask 버전을 갱신
    
    - DMG `sha256` 값을 반영

--- Casks/bizbox-notch.rb
+++ Casks/bizbox-notch.rb
@@ -1,6 +1,6 @@
 cask "bizbox-notch" do
-  version "0.2.32"
-  sha256 "bfafe765462a27bbe03840e7185d79778db64e52bc36f5721715d8a4b14a4276"
+  version "0.2.33"
+  sha256 "207a9c08651cc085ad9276df17e68fb4362ff58cbccabe50e58e35e42367a81e"
 
   url "https://github.com/hahmjuntae/bizbox-notch/releases/download/v#{version}/Bizbox-Notch-#{version}.dmg"
   name "Bizbox Notch"
